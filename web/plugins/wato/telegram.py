ff59f1a72cead36c4c28c70f1af199e7dd785337
Stefan Gehn <stefan@srcbox.net>
2021-01-03 17:26:36 +0100

    Use Checkmk notification parameters
    Switch configuration from custom contact variables and parameters
    to Checkmk parameters that can be edited in WATO. The previous setup
    is rather tedious for users and there's always a chance of the
    notification rule sending to users that do not have their Telegram
    Chat ID set.
    
    This is inspired by the configuration of other push notification types
    already shipped with Checkmk, i.e. slack or pushover.

---
+++ web/plugins/wato/telegram.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+# SPDX-FileCopyrightText: 2021 Stefan Gehn <stefan+cmk@srcbox.net>
+#
+# SPDX-License-Identifier: GPL-2.0-only
+
+from cmk.gui.valuespec import Dictionary, TextAscii
+from cmk.gui.plugins.wato import notification_parameter_registry, NotificationParameter
+
+
+@notification_parameter_registry.register
+class NotificationParameterTelegram(NotificationParameter):
+    @property
+    def ident(self):
+        return "telegram"
+
+    @property
+    def spec(self):
+        return Dictionary(
+            title=_("Create notification with the following parameters"),
+            required_keys=["bot_token", "chat_id"],
+            elements=[
+                (
+                    "bot_token",
+                    TextAscii(
+                        title=_("Bot Token"),
+                        help=_("Telegram Bot Token for sending notifications"),
+                        size=46,
+                        allow_empty=False,
+                    ),
+                ),
+                (
+                    "chat_id",
+                    TextAscii(
+                        title=_("Chat ID"),
+                        help=_("Telegram Chat ID to send notifications to"),
+                        size=24,
+                        allow_empty=False,
+                    ),
+                ),
+            ],
+        )
